50d95c84cec0ba97a3de1c883edf5aced336e765
AgnieszkaaK <20106558@mail.wit.ie>
2025-12-09 02:55:26 +0000

    Added userId to tasks

--- tasky-api/api/tasks/index.js
+++ tasky-api/api/tasks/index.js
@@ -2,19 +2,32 @@ import express from 'express';
 import Task from './taskModel';
 import asyncHandler from 'express-async-handler';
 
-const router = express.Router();  
+const router = express.Router(); // eslint-disable-line
 
+// Get a user's tasks
+router.get('/user/:uid', async (req, res) => {
+    const tasks = await Task.find({ userId: `${req.params.uid}`});
+    res.status(200).json(tasks);
+});
+// Get all tasks
 // Get all tasks
 router.get('/', async (req, res) => {
-    const tasks = await Task.find();
+    const tasks = await Task.find().populate('userId', 'username');
+    res.status(200).json(tasks);
+});
+
+// Get a user's tasks
+router.get('/user/:uid', async (req, res) => {
+    const tasks = await Task.find({ userId: `${req.params.uid}`});
     res.status(200).json(tasks);
 });
 
 // create a task
-router.post('/', asyncHandler(async (req, res) => {
+router.post('/', async (req, res) => {
     const task = await Task(req.body).save();
     res.status(201).json(task);
-}));
+});
+
 // Update Task
 router.put('/:id', async (req, res) => {
     if (req.body._id) delete req.body._id;
